1acc81c65dabab8ff240af4f068d1b51518692e2
JbIPS <jeanbaptiste.richardet@gmail.com>
2022-03-31 18:30:07 +0200

    Add image captions
    * Show image title as caption
    * Reduce quote size
    * Make image float left

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,11 @@ const pluginNavigation = require("@11ty/eleventy-navigation");
 const Image = require('@11ty/eleventy-img');
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const markdownItBlock = require("markdown-it-block-image");
+const markdownItCaption = require("markdown-it-image-figures");
 const metagen = require('eleventy-plugin-metagen');
 const criticalCss = require('eleventy-critical-css');
 const htmlmin = require('html-minifier');
-const {minify} = require('terser');
+const { minify } = require('terser');
 
 const DEFAULT_DOMAIN = 'localhost:8080';
 process.setMaxListeners(0)
@@ -26,7 +26,7 @@ module.exports = function(eleventyConfig) {
 
   const makeAbsolute = (url) => {
     let domain, protocol;
-    if(process.env.DOMAIN) {
+    if (process.env.DOMAIN) {
       domain = process.env.DOMAIN;
       protocol = 'https://';
     } else {
@@ -71,7 +71,7 @@ module.exports = function(eleventyConfig) {
     const url = makeAbsolute(this.page.url);
     const account = social.account;
     const sharePayload = eval('`' + social.post_url + '`')
-    return `<a target="_blank" class="social" href="${ share ? sharePayload : social.url }">
+    return `<a target="_blank" class="social" href="${share ? sharePayload : social.url}">
         ${content}
       </a>`
   })
@@ -81,7 +81,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addFilter("date", (dateObj, format) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat(format);
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(format);
   });
 
   //eleventyConfig.addFilter("slug", str => {
@@ -96,7 +96,7 @@ module.exports = function(eleventyConfig) {
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
+    if (n < 0) {
       return array.slice(n);
     }
 
@@ -130,7 +130,7 @@ module.exports = function(eleventyConfig) {
 
   /* Minify HTML */
   eleventyConfig.addTransform("htmlmin", function(content) {
-    if(this.outputPath.endsWith(".html") ) {
+    if (this.outputPath.endsWith(".html")) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
@@ -143,10 +143,10 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.on('afterBuild', async () => {
     const code = {
-      'splide.js': await fs.readFile('js/splide.js', {encoding: 'utf-8'}),
-      'shuffle.min.js': await fs.readFile('js/shuffle.min.js', {encoding: 'utf-8'}),
-      'isotope.js': await fs.readFile('js/isotope.pkgd.min.js', {encoding: 'utf-8'}),
-      'index.js': await fs.readFile('js/index.js', {encoding: 'utf-8'}),
+      'splide.js': await fs.readFile('js/splide.js', { encoding: 'utf-8' }),
+      'shuffle.min.js': await fs.readFile('js/shuffle.min.js', { encoding: 'utf-8' }),
+      'isotope.js': await fs.readFile('js/isotope.pkgd.min.js', { encoding: 'utf-8' }),
+      'index.js': await fs.readFile('js/index.js', { encoding: 'utf-8' }),
     };
     const result = await minify(code);
     await Promise.all([
@@ -163,32 +163,34 @@ module.exports = function(eleventyConfig) {
     html: true,
     breaks: false,
     linkify: true
-  }).use(markdownItAnchor, {
-    permalink: false,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  }).use(markdownItBlock);
+  })
+    .use(markdownItAnchor, {
+      permalink: false,
+      permalinkClass: "direct-link",
+      permalinkSymbol: "#"
+    })
+    .use(markdownItCaption, {
+      figcaption: true,
+      lazy: true,
+      async: true,
+    });
+  const src = 'text with ![](img.png "Nin/e")\n\n![](fig.png "NItle")\n\nanother paragraph';
+  const res = markdownLibrary.render(src);
+
+  console.log(res);
   // Remember old renderer, if overridden, or proxy to default renderer
   const defaultRender = markdownLibrary.renderer.rules.link_open || function(tokens, idx, options, env, self) {
     return self.renderToken(tokens, idx, options);
   };
 
-  markdownLibrary.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  markdownLibrary.renderer.rules.link_open = function(tokens, idx, options, env, self) {
     // If link URL is outside of this domain
-    if(tokens[idx].attrGet('href').startsWith('http'))
+    if (tokens[idx].attrGet('href').startsWith('http'))
       tokens[idx].attrPush(['target', '_blank']); // add new attribute
 
     // pass token to default renderer.
     return defaultRender(tokens, idx, options, env, self);
   };
-
-  markdownLibrary.renderer.rules.image = function (tokens, idx, options, env, slf) {
-    var token = tokens[idx]
-    token.attrs[token.attrIndex('alt')][1] = slf.renderInlineAsText(token.children, options, env)
-    // this is the line of code responsible for an additional 'loading' attribute
-    token.attrSet('loading', 'lazy');
-    return slf.renderToken(tokens, idx, options);
-  }
 
   eleventyConfig.setLibrary("md", markdownLibrary);
   eleventyConfig.addFilter('markdown', (string, strip) => {
